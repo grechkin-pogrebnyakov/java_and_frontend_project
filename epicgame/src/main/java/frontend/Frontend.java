daab1fab6bf7688788011ea64b4692a2167c1f69
grechkin-pogrebnyakov <grechkin.pogrebnyakov@gmail.com>
2014-09-15 20:38:35 +0400

    epicgame_v0.0

---
+++ epicgame/src/main/java/frontend/Frontend.java
@@ -0,0 +1,36 @@
+package frontend;
+
+import templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author v.chibrikov
+ */
+public class Frontend extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("email", email == null ? "" : email);
+        pageVariables.put("password", password == null ? "" : password);
+
+        response.getWriter().println(PageGenerator.getPage("authresponse.txt", pageVariables));
+    }
+}
